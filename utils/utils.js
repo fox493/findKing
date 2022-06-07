f563e915f54ff95676fe8e9a76f1de3f17aba7e0
TimGrey998 <timgrey998@gmail.com>
2022-06-07 22:00:48 +0800

    change structure

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 import { printBanner } from "./banner.js"
-import { config } from "./config.js"
+import { config } from "../config.js"
 import { ethers } from "ethers"
 import dotenv from "dotenv"
 import fs from "fs"
